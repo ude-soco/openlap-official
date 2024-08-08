d4ef8e93cad3ee4c90a99bf119e7d8ffb1577574
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-08-08 16:13:42 +0200

    Add theme context

--- openlap-indicatoreditor/src/App.jsx
+++ openlap-indicatoreditor/src/App.jsx
@@ -1,37 +1,13 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {AuthProvider} from "./setup/auth-context-manager/auth-context-manager.jsx";
-import PrivateRoute from "./setup/routes-manager";
-import UserProfile from "./pages/account-manager/user-profile";
-import Login from "./pages/login/login";
-import Register from "./pages/register/register";
-import RoleTypes from "./common/enums/role-types";
+import AppRoutes from "./setup/routes-manager/app-routes.jsx";
+import { CustomThemeProvider } from "./setup/theme-manager/theme-context-manager.jsx";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <AuthProvider>
-        <Router>
-          <Routes>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
-            <Route
-              path="/"
-              element={
-                <PrivateRoute
-                  component={<UserProfile/>}
-                  allowedRoles={[
-                    RoleTypes.user,
-                    RoleTypes["data provider"],
-                    RoleTypes.admin,
-                  ]}
-                />
-              }
-            />
-          </Routes>
-        </Router>
-      </AuthProvider>
-    </>
+    <CustomThemeProvider>
+
+        <AppRoutes />
+    </CustomThemeProvider>
   );
-}
+};
 
 export default App;
