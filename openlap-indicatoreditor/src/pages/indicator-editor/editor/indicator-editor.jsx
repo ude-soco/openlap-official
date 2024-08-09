9c5c966b3312c578e3f04bbb5ad7551f0602b20c
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-08-10 01:55:56 +0200

    Responsive indicator editor

--- openlap-indicatoreditor/src/pages/indicator-editor/editor/indicator-editor.jsx
+++ openlap-indicatoreditor/src/pages/indicator-editor/editor/indicator-editor.jsx
@@ -1,8 +1,11 @@
-import { Grid, Typography, Paper } from "@mui/material";
+import { Grid, Typography, useMediaQuery, useTheme } from "@mui/material";
 import PreviewPanel from "./components/preview-panel/preview-panel";
 import SelectionPanel from "./components/selection-panel/selection-panel";
 
 const IndicatorEditor = () => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("lg"));
+
   return (
     <>
       <Grid container spacing={2}>
@@ -11,12 +14,25 @@ const IndicatorEditor = () => {
         </Grid>
         <Grid item xs={12}>
           <Grid container spacing={2}>
-            <Grid item sx={{ maxWidth: "550px", width: "100%" }}>
-              <PreviewPanel />
-            </Grid>
-            <Grid item xs={12} lg>
-              <SelectionPanel />
-            </Grid>
+            {isSmallScreen ? (
+              <>
+                <Grid item xs={12} lg={8}>
+                  <SelectionPanel />
+                </Grid>
+                <Grid item xs={12} lg={4}>
+                  <PreviewPanel />
+                </Grid>
+              </>
+            ) : (
+              <>
+                <Grid item xs={12} lg={4}>
+                  <PreviewPanel />
+                </Grid>
+                <Grid item xs={12} lg={8}>
+                  <SelectionPanel />
+                </Grid>
+              </>
+            )}
           </Grid>
         </Grid>
       </Grid>
