6f6c0911fca0d365199e81c5d0dd80bd6c08c09e
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-08-10 22:40:32 +0200

    Add visualization to preview panel

--- openlap-indicatoreditor/src/pages/indicator-editor/editor/components/selection-panel/components/visualization/components/inputs.jsx
+++ openlap-indicatoreditor/src/pages/indicator-editor/editor/components/selection-panel/components/visualization/components/inputs.jsx
@@ -164,10 +164,6 @@ const Inputs = ({ state, setState }) => {
             </Grid>
           );
         })}
-
-        <Grid item xs={12} sx={{ py: 2 }}>
-          <Divider />
-        </Grid>
       </Grid>
     </>
   );
