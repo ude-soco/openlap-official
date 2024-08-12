13db378a01d0ca8386f57296b69bd55a14893ff0
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-08-12 09:54:48 +0200

    Add indicator preview page

--- openlap-indicatoreditor/src/pages/indicator-editor/dashboard/utils/indicator-dashboard.js
+++ openlap-indicatoreditor/src/pages/indicator-editor/dashboard/utils/indicator-dashboard.js
@@ -1,3 +1,5 @@
+import parse from "html-react-parser";
+
 export const fetchMyIndicators = async (api, params = {}) => {
   try {
     const queryString = new URLSearchParams(params).toString();
@@ -5,7 +7,44 @@ export const fetchMyIndicators = async (api, params = {}) => {
     return response.data.data;
   } catch (error) {
     console.error("Failed to fetch user's indicators list data");
-    console.log(error);
+    throw error; // Re-throw the error to handle it in the component
+  }
+};
+
+export const fetchIndicatorFullDetail = async (api, indicatorId) => {
+  try {
+    const response = await api.get(`v1/indicators/${indicatorId}`);
+    const unescapedVizCode = decodeURIComponent(
+      response.data.data.indicatorCode
+    );
+    let displayCode = parse(unescapedVizCode);
+    let scriptData;
+    try {
+      scriptData = displayCode[1].props.dangerouslySetInnerHTML.__html;
+    } catch (error) {
+      console.error("Error script code", error);
+    }
+
+    let updatedResponse = {
+      ...response.data.data,
+      indicatorCode: {
+        displayCode,
+        scriptData,
+      },
+    };
+    return updatedResponse;
+  } catch (error) {
+    console.error("Failed to fetch indicators code data");
+    throw error; // Re-throw the error to handle it in the component
+  }
+};
+
+export const fetchRequestIndicatorCode = async (api, indicatorId) => {
+  try {
+    const response = await api.get(`v1/indicators/${indicatorId}/code`);
+    return response.data.data;
+  } catch (error) {
+    console.error("Failed to fetch indicators code data");
     throw error; // Re-throw the error to handle it in the component
   }
 };
--- openlap-indicatoreditor/src/setup/routes-manager/app-routes.jsx
+++ openlap-indicatoreditor/src/setup/routes-manager/app-routes.jsx
@@ -24,6 +24,8 @@ import GQIEditor from "../../pages/gqi-editor/gqi-editor/gqi-editor.jsx";
 import GQIPool from "../../pages/gqi-editor/gqi-pool/gqi-pool.jsx";
 import { DrawerHeader } from "../../common/components/side-bar/side-bar.jsx";
 import Footer from "../../common/components/footer/footer.jsx";
+import { SnackbarProvider } from "notistack";
+import IndicatorPreview from "../../pages/indicator-editor/dashboard/indicator-preview.jsx";
 
 const drawerWidth = 280;
 
@@ -64,138 +66,152 @@ const AppRoutes = () => {
   return (
     <>
       <ThemeProvider theme={darkMode ? themeDark : themeLight}>
-        <CssBaseline />
-        <Router>
-          <Box sx={{ display: "flex" }}>
-            {Boolean(user) && (
-              <NavBar openSidebar={openSidebar} toggleSidebar={toggleSidebar} />
-            )}
-            {Boolean(user) && <Sidebar openSidebar={openSidebar} />}
-            <Grid container justifyContent="center">
-              <Main
-                open={openSidebar}
-                sx={{ maxWidth: "1920px", width: "100%" }}
-              >
-                <DrawerHeader />
-                <Paper
-                  elevation={0}
-                  sx={{
-                    minHeight: "100vh",
-                    p: 2,
-                    width: "100%",
-                  }}
+        <SnackbarProvider maxSnack={3}>
+          <CssBaseline />
+          <Router>
+            <Box sx={{ display: "flex" }}>
+              {Boolean(user) && (
+                <NavBar
+                  openSidebar={openSidebar}
+                  toggleSidebar={toggleSidebar}
+                />
+              )}
+              {Boolean(user) && <Sidebar openSidebar={openSidebar} />}
+              <Grid container justifyContent="center">
+                <Main
+                  open={openSidebar}
+                  sx={{ maxWidth: "1920px", width: "100%" }}
                 >
-                  <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route
-                      path="/"
-                      element={
-                        <PrivateRoute
-                          component={<UserProfile />}
-                          allowedRoles={[
-                            RoleTypes.user,
-                            RoleTypes["data provider"],
-                            RoleTypes.admin,
-                          ]}
+                  <DrawerHeader />
+                  <Paper
+                    elevation={0}
+                    sx={{
+                      minHeight: "100vh",
+                      p: 2,
+                      width: "100%",
+                    }}
+                  >
+                    <Routes>
+                      <Route path="/login" element={<Login />} />
+                      <Route path="/register" element={<Register />} />
+                      <Route
+                        path="/"
+                        element={
+                          <PrivateRoute
+                            component={<UserProfile />}
+                            allowedRoles={[
+                              RoleTypes.user,
+                              RoleTypes["data provider"],
+                              RoleTypes.admin,
+                            ]}
+                          />
+                        }
+                      />
+                      <Route path="/indicator">
+                        <Route
+                          index
+                          element={
+                            <PrivateRoute
+                              component={<IndicatorEditorDashboard />}
+                              allowedRoles={[RoleTypes.user]}
+                            />
+                          }
                         />
-                      }
-                    />
-                    <Route path="/indicator">
-                      <Route
-                        index
-                        element={
-                          <PrivateRoute
-                            component={<IndicatorEditorDashboard />}
-                            allowedRoles={[RoleTypes.user]}
-                          />
-                        }
-                      />
-                      <Route
-                        path="editor"
-                        element={
-                          <PrivateRoute
-                            component={<IndicatorEditor />}
-                            allowedRoles={[RoleTypes.user]}
-                          />
-                        }
-                      />
-                      <Route
-                        path="pool"
-                        element={
-                          <PrivateRoute
-                            component={<IndicatorPool />}
-                            allowedRoles={[RoleTypes.user]}
-                          />
-                        }
-                      />
-                    </Route>
-                    <Route path="/isc">
-                      <Route
-                        index
-                        element={
-                          <PrivateRoute
-                            component={<ISCDashboard />}
-                            allowedRoles={[RoleTypes.user]}
-                          />
-                        }
-                      />
-                      <Route
-                        path="creator"
-                        element={
-                          <PrivateRoute
-                            component={<ISCCreator />}
-                            allowedRoles={[RoleTypes.user]}
-                          />
-                        }
-                      />
-                      <Route
-                        path="pool"
-                        element={
-                          <PrivateRoute
-                            component={<ISCPool />}
-                            allowedRoles={[RoleTypes.user]}
-                          />
-                        }
-                      />
-                    </Route>
+                        <Route
+                          path="editor"
+                          element={
+                            <PrivateRoute
+                              component={<IndicatorEditor />}
+                              allowedRoles={[RoleTypes.user]}
+                            />
+                          }
+                        />
+                        <Route
+                          path=":id"
+                          element={
+                            <PrivateRoute
+                              component={<IndicatorPreview />}
+                              allowedRoles={[RoleTypes.user]}
+                            />
+                          }
+                        />
+                        <Route
+                          path="pool"
+                          element={
+                            <PrivateRoute
+                              component={<IndicatorPool />}
+                              allowedRoles={[RoleTypes.user]}
+                            />
+                          }
+                        />
+                      </Route>
+                      <Route path="/isc">
+                        <Route
+                          index
+                          element={
+                            <PrivateRoute
+                              component={<ISCDashboard />}
+                              allowedRoles={[RoleTypes.user]}
+                            />
+                          }
+                        />
+                        <Route
+                          path="creator"
+                          element={
+                            <PrivateRoute
+                              component={<ISCCreator />}
+                              allowedRoles={[RoleTypes.user]}
+                            />
+                          }
+                        />
+                        <Route
+                          path="pool"
+                          element={
+                            <PrivateRoute
+                              component={<ISCPool />}
+                              allowedRoles={[RoleTypes.user]}
+                            />
+                          }
+                        />
+                      </Route>
 
-                    <Route path="/gqi">
-                      <Route
-                        index
-                        element={
-                          <PrivateRoute
-                            component={<GQIDashboard />}
-                            allowedRoles={[RoleTypes.user]}
-                          />
-                        }
-                      />
-                      <Route
-                        path="editor"
-                        element={
-                          <PrivateRoute
-                            component={<GQIEditor />}
-                            allowedRoles={[RoleTypes.user]}
-                          />
-                        }
-                      />
-                      <Route
-                        path="pool"
-                        element={
-                          <PrivateRoute
-                            component={<GQIPool />}
-                            allowedRoles={[RoleTypes.user]}
-                          />
-                        }
-                      />
-                    </Route>
-                  </Routes>
-                </Paper>
-              </Main>
-              <Footer />
-            </Grid>
-          </Box>
-        </Router>
+                      <Route path="/gqi">
+                        <Route
+                          index
+                          element={
+                            <PrivateRoute
+                              component={<GQIDashboard />}
+                              allowedRoles={[RoleTypes.user]}
+                            />
+                          }
+                        />
+                        <Route
+                          path="editor"
+                          element={
+                            <PrivateRoute
+                              component={<GQIEditor />}
+                              allowedRoles={[RoleTypes.user]}
+                            />
+                          }
+                        />
+                        <Route
+                          path="pool"
+                          element={
+                            <PrivateRoute
+                              component={<GQIPool />}
+                              allowedRoles={[RoleTypes.user]}
+                            />
+                          }
+                        />
+                      </Route>
+                    </Routes>
+                  </Paper>
+                <Footer />
+                </Main>
+              </Grid>
+            </Box>
+          </Router>
+        </SnackbarProvider>
       </ThemeProvider>
     </>
   );
