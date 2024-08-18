9c90f107ba4afeb3e4c20370276f888e43a27d5f
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-08-18 18:14:12 +0200

    Add stacked bar chart component

---
+++ openlap-indicatoreditor/src/pages/indicator-specification-cards/creator/components/finalize/components/scatter-plot-chart.jsx
@@ -0,0 +1,282 @@
+import React, { useContext, useEffect, useState } from "react";
+import { CustomThemeContext } from "../../../../../../setup/theme-manager/theme-context-manager.jsx";
+import { ISCContext } from "../../../indicator-specification-card.jsx";
+import Chart from "react-apexcharts";
+import { FormControl, Grid, InputLabel, MenuItem, Select } from "@mui/material";
+
+const ScatterPlotChart = () => {
+  const { darkMode } = useContext(CustomThemeContext);
+  const { dataset, visRef } = useContext(ISCContext);
+
+  const [state, setState] = useState({
+    series: [],
+    options: {
+      chart: {
+        type: visRef.chart.code,
+        zoom: {
+          type: "xy",
+        },
+        foreColor: darkMode ? "#ffffff" : "#000000",
+      },
+      xaxis: {
+        title: {
+          text: "X Axis",
+        },
+        categories: [],
+      },
+      yaxis: {
+        title: {
+          text: "Y Axis",
+        },
+      },
+      tooltip: {
+        enabled: true,
+        followCursor: true,
+        theme: darkMode ? "dark" : "light",
+        x: {
+          formatter: function (val) {
+            return `X: ${val}`;
+          },
+        },
+        y: {
+          formatter: function (val) {
+            return `Y: ${val}`;
+          },
+        },
+        z: {
+          formatter: function (val) {
+            return `Label: ${val}`;
+          },
+        },
+      },
+    },
+    axisOptions: {
+      xAxisOptions: [],
+      yAxisOptions: [],
+      labelOptions: [],
+      selectedXAxis: "",
+      selectedYAxis: "",
+      selectedLabel: "",
+    },
+  });
+
+  useEffect(() => {
+    if (dataset && dataset.rows && dataset.columns) {
+      const stringColumns = dataset.columns.filter(
+        (col) => col.type === "string",
+      );
+      const numberColumns = dataset.columns.filter(
+        (col) => col.type === "number",
+      );
+
+      setState((prevState) => ({
+        ...prevState,
+        axisOptions: {
+          xAxisOptions: numberColumns,
+          yAxisOptions: numberColumns,
+          labelOptions: stringColumns,
+          selectedXAxis:
+            prevState.axisOptions.selectedXAxis ||
+            (numberColumns.length > 0 ? numberColumns[0].field : ""),
+          selectedYAxis:
+            prevState.axisOptions.selectedYAxis ||
+            (numberColumns.length > 0
+              ? numberColumns[1]?.field
+              : numberColumns[0]?.field || ""),
+          selectedLabel:
+            prevState.axisOptions.selectedLabel ||
+            (stringColumns.length > 0 ? stringColumns[0].field : ""),
+        },
+      }));
+    }
+  }, [dataset, darkMode, visRef.chart.code]);
+
+  useEffect(() => {
+    if (dataset && dataset.rows && dataset.columns) {
+      const { selectedXAxis, selectedYAxis, selectedLabel } = state.axisOptions;
+      const xColumn = dataset.columns.find(
+        (col) => col.field === selectedXAxis,
+      );
+      const yColumn = dataset.columns.find(
+        (col) => col.field === selectedYAxis,
+      );
+      const labelColumn = dataset.columns.find(
+        (col) => col.field === selectedLabel,
+      );
+
+      const defaultData = [{ x: 0, y: 0, label: "Default Point" }];
+
+      const data =
+        dataset.rows.map((row) => ({
+          x: xColumn ? row[xColumn.field] || 0 : 0,
+          y: yColumn ? row[yColumn.field] || 0 : 0,
+          label: labelColumn ? row[labelColumn.field] || "Unknown" : "Unknown",
+        })) || defaultData;
+
+      // Calculate min and max for the x-axis for setting the range
+      const xValues = data.map((item) => item.x);
+      const minX = Math.min(...xValues);
+      const maxX = Math.max(...xValues);
+
+      setState((prevState) => ({
+        ...prevState,
+        series: [
+          {
+            name: "Scatter Series",
+            data: data.map((item) => ({
+              x: item.x,
+              y: item.y,
+              z: item.label,
+            })),
+          },
+        ],
+        options: {
+          ...prevState.options,
+          xaxis: {
+            ...prevState.options.xaxis,
+            title: {
+              text: xColumn ? xColumn.headerName : "X Axis",
+            },
+            tickAmount: 6, // This sets the number of ticks (labels) on the x-axis
+            min: minX, // Minimum value on the x-axis
+            max: maxX, // Maximum value on the x-axis
+            labels: {
+              formatter: function (val) {
+                return parseFloat(val).toFixed(2); // Format the labels with 2 decimal points
+              },
+            },
+          },
+          yaxis: {
+            ...prevState.options.yaxis,
+            title: {
+              text: yColumn ? yColumn.headerName : "Y Axis",
+            },
+          },
+          chart: {
+            ...prevState.options.chart,
+            foreColor: darkMode ? "#ffffff" : "#000000",
+          },
+          tooltip: {
+            ...prevState.options.tooltip,
+            theme: darkMode ? "dark" : "light",
+          },
+        },
+      }));
+    }
+  }, [
+    dataset,
+    state.axisOptions.selectedXAxis,
+    state.axisOptions.selectedYAxis,
+    state.axisOptions.selectedLabel,
+    darkMode,
+    visRef.chart.code,
+  ]);
+
+  const handleXAxisChange = (event) => {
+    setState((prevState) => ({
+      ...prevState,
+      axisOptions: {
+        ...prevState.axisOptions,
+        selectedXAxis: event.target.value,
+      },
+    }));
+  };
+
+  const handleYAxisChange = (event) => {
+    setState((prevState) => ({
+      ...prevState,
+      axisOptions: {
+        ...prevState.axisOptions,
+        selectedYAxis: event.target.value,
+      },
+    }));
+  };
+
+  const handleLabelChange = (event) => {
+    setState((prevState) => ({
+      ...prevState,
+      axisOptions: {
+        ...prevState.axisOptions,
+        selectedLabel: event.target.value,
+      },
+    }));
+  };
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <FormControl fullWidth>
+                <InputLabel id="x-axis-select-label">X Axis</InputLabel>
+                <Select
+                  labelId="x-axis-select-label"
+                  id="x-axis-select"
+                  value={state.axisOptions.selectedXAxis}
+                  onChange={handleXAxisChange}
+                  label="X Axis"
+                  variant="outlined"
+                >
+                  {state.axisOptions.xAxisOptions.map((col) => (
+                    <MenuItem key={col.field} value={col.field}>
+                      {col.headerName}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={4}>
+              <FormControl fullWidth>
+                <InputLabel id="y-axis-select-label">Y Axis</InputLabel>
+                <Select
+                  labelId="y-axis-select-label"
+                  id="y-axis-select"
+                  value={state.axisOptions.selectedYAxis}
+                  onChange={handleYAxisChange}
+                  label="Y Axis"
+                  variant="outlined"
+                >
+                  {state.axisOptions.yAxisOptions.map((col) => (
+                    <MenuItem key={col.field} value={col.field}>
+                      {col.headerName}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={4}>
+              <FormControl fullWidth>
+                <InputLabel id="label-select-label">Labels</InputLabel>
+                <Select
+                  labelId="label-select-label"
+                  id="label-select"
+                  value={state.axisOptions.selectedLabel}
+                  onChange={handleLabelChange}
+                  label="Labels"
+                  variant="outlined"
+                >
+                  {state.axisOptions.labelOptions.map((col) => (
+                    <MenuItem key={col.field} value={col.field}>
+                      {col.headerName}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sx={{ minHeight: 600 }}>
+          <Chart
+            options={state.options}
+            series={state.series}
+            type={visRef.chart.code}
+            height="100%"
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default ScatterPlotChart;
--- openlap-indicatoreditor/src/pages/indicator-specification-cards/creator/utils/data/config.js
+++ openlap-indicatoreditor/src/pages/indicator-specification-cards/creator/utils/data/config.js
@@ -141,7 +141,7 @@ const visualizations = [
     fullDescription:
       'Also known as Bar Graph or Column Graph. \n\n A Bar Chart uses either horizontal or vertical bars (column chart) to show discrete, numerical comparisons across categories. One axis of the chart shows the specific categories being compared and the other axis represents a discrete value scale. \n\n  Bar Charts are distinguished from Histograms, as they do not display continuous developments over an interval. Instead, Bar Chart\'s discrete data is categorical and therefore answers the question of "how many?" in each category.',
     description:
-      "A bar chart is used to compare values across a few categories. Use it when the order of categories is not important.",
+      "A bar chart uses rectangular bars to compare values across different categories, with bar height or length representing the data's value. \n\n Another variant of bar chart is a grouped bar chart. It compares multiple categories across different groups using clusters of bars for each group.",
     imageDescription: BarChartDescriptionImage,
     imageFullDescription: BarChartFullDescriptionImage,
     link: "https://datavizcatalogue.com/methods/bar_chart.html",
@@ -149,7 +149,7 @@ const visualizations = [
   {
     image: ScatterPlotImage,
     enable: true,
-    type: VisualizationTypes.dot,
+    type: VisualizationTypes.scatter,
     code: "scatter",
     filters: [ChartTypes.trends],
     dataTypes: [
@@ -159,24 +159,24 @@ const visualizations = [
       },
       {
         type: DataTypes.numerical,
-        required: 1,
+        required: 2,
       },
       {
         type: DataTypes.catOrdered,
-        required: 1,
+        required: 0,
       },
     ],
     fullDescription:
-      "A dot chart, also known as a dot plot, dot graph, or dot diagram, is a simple data visualization tool used to display the distribution or frequency of a dataset. It is particularly useful for visualizing categorical or discrete data, such as the distribution of values within a category or the occurrence of specific events. \n\n Dot charts are particularly useful for showing the distribution of data when you want to emphasize individual data points rather than connecting them with lines. They are often used in data analysis, statistics, and data visualization to provide a clear and concise representation of the dataset.",
+      "A scatter plot chart, also known as a dot plot, dot graph, or dot diagram, is a simple data visualization tool used to display the distribution or frequency of a dataset. It is particularly useful for visualizing categorical or discrete data, such as the distribution of values within a category or the occurrence of specific events. \n\n Dot charts are particularly useful for showing the distribution of data when you want to emphasize individual data points rather than connecting them with lines. They are often used in data analysis, statistics, and data visualization to provide a clear and concise representation of the dataset.",
     imageDescription: ScatterPlotDescriptionImage,
     imageFullDescription: ScatterPlotFullDescriptionImage,
     link: "https://datavizcatalogue.com/methods/scatterplot.html",
     description:
-      "A dot chart is a visual representation using dots to show the distribution or frequency of data points in different categories or values.",
+      "A scatter plot displays individual data points on a grid, showing the relationship between two variables along the x and y axes.",
   },
   {
     image: GroupedBarChartImage,
-    enable: true,
+    enable: false,
     type: VisualizationTypes.groupedBar,
     code: "bar",
     filters: [ChartTypes.trends, ChartTypes.relationship],
@@ -197,7 +197,7 @@ const visualizations = [
     fullDescription:
       "Also known as a Grouped Bar Chart or Clustered Bar Chart. This variation of a Bar Chart can be used when two or more data series need to be plotted all on the same axis and grouped into parent categories. \n\n Like on a Bar Chart, the length of each bar on a Multiset Bar Chart is used to show discrete, numerical comparisons amongst categories. Each bar for a data series is assigned a colour to distinguish them apart. Bars in the same group are placed together and are then spaced apart from other bar groupings. \n\n The use of Multiset Bar Charts is usually to compare across categories that contain the same sub-categorical variables between them. Each bar is a subcategory that is grouped into a larger parent category. \n\n Multiset Bar Charts can also be used to compare mini Histograms to each other, so each bar in the group would represent the significant intervals of a variable. Another use could be to use Multiset Bar Charts to show data changing over time by having, for example, each bar represent a point in time such as a year. \n\n The downside of Multiset Bar Charts is that they become harder to read the more bars you have in one group. Therefore, you should try to limit the number of bars per group.",
     description:
-      "Grouped bar chart is used to compare values across a few categories. Use it when the order of categories is not important.",
+      "A grouped bar chart compares multiple categories across different groups using clusters of bars for each group.",
     imageDescription: GroupedBarChartDescriptionImage,
     imageFullDescription: GroupedBarChartFullDescriptionImage,
     link: "https://datavizcatalogue.com/methods/multiset_barchart.html",
@@ -225,7 +225,7 @@ const visualizations = [
     fullDescription:
       "A line chart is used to display quantitative values over a continuous interval or time period. A Line Graph is most frequently used to show trends and analyse how the data has changed over time. \n\n Line Graphs are drawn by first plotting data points on a Cartesian coordinate grid, and then connecting a line between all of these points. Typically, the y-axis has a quantitative value, while the x-axis is a timescale or a sequence of intervals. Negative values can be displayed below the x-axis. \n\n  The direction of the lines on the graph works as a nice metaphor for the data: an upward slope indicates where values have increased and a downward slope indicates where values have decreased. The line's journey across the graph can create patterns that reveal trends in a dataset. \n\nWhen grouped with other lines (other data series), individual lines can be compared to one another. However, avoid using more than 3-4 lines per graph, as this makes the chart more cluttered and harder to read. A solution to this is to divide the chart into smaller multiples (have a small Line Graph for each data series).",
     description:
-      "A line chart is used to display trends over time (years, months, and days) or categories when the order is important. Use it when there are many data points and the order is important.",
+      "A line chart shows data trends over time by connecting individual data points with a continuous line.",
     imageDescription: LineChartDescriptionImage,
     imageFullDescription: LineChartFullDescriptionImage,
     link: "https://datavizcatalogue.com/methods/line_graph.html",
@@ -253,7 +253,7 @@ const visualizations = [
     fullDescription:
       "Also known as a Coxcomb Chart, Nightingale Rose Chart. \n\n This chart was famously used by statistician and medical reformer, Florence Nightingale to communicate the avoidable deaths of soldiers during the Crimean war. \n\n  Polar area charts are drawn on a polar coordinate grid. Each category or interval in the data is divided into equal segments on this radial chart. How far each segment extends from the centre of the polar axis depends on the value it represents. So each ring from the centre of the polar grid can be used as a scale to plot the segment size and represent a higher value. \n\n The major flaw with Nightingale Rose Charts is that the outer segments are emphasised more because of their larger area size, which disproportionately represents any value increases.",
     description:
-      "The Polar Area chart is similar to a usual pie chart, except sectors are equal angles and differ rather in how far each sector extends from the center of the circle. The polar area diagram is used to plot cyclic phenomena (e.g., count of deaths by month).",
+      "The Polar Area chart displays data in a circular format, with segments representing values at different angles from the center.",
     imageDescription: PolarAreaChartDescriptionImage,
     imageFullDescription: PolarAreaChartFullDescriptionImage,
     link: "https://datavizcatalogue.com/methods/nightingale_rose_chart.html",
@@ -281,7 +281,7 @@ const visualizations = [
     fullDescription:
       "Extensively used in presentations and offices, Pie Charts help show proportions and percentages between categories, by dividing a circle into proportional segments. Each arc length represents a proportion of each category, while the full circle represents the total sum of all the data, equal to 100%. \n\n Pie Charts are ideal for giving the reader a quick idea of the proportional distribution of the data. However, the major downsides to pie charts are: \n\n  They cannot show more than a few values, because as the number of values shown increases, the size of each segment/slice becomes smaller. This makes them unsuitable for large datasets with many categories. \n\n They take up more space than their alternatives, for example, a 100% Stacked Bar Chart. Mainly due to their size and the usual need for a legend. \n\n They are not great for making accurate comparisons between groups of Pie Charts. This is because it is harder to distinguish the size of items via area when it is for length. \n\n Despite that, comparing a given category (one slice) within the total of a single Pie Chart, then it can often be more effective.",
     description:
-      "A pie chart is used to show proportions of a whole. Use it to show numbers that relate to a larger sum and always equal to 100%. Do not use this chart if it contains many slices as angles are hard to estimate",
+      "A pie chart represents parts of a whole with slices, where each slice's size indicates its proportion of the total.",
     imageDescription: PieChartDescriptionImage,
     imageFullDescription: PieChartFullDescriptionImage,
     link: "https://datavizcatalogue.com/methods/pie_chart.html",
@@ -309,7 +309,7 @@ const visualizations = [
     fullDescription:
       "Stacked Bar Graphs segment the bars on top of each other. They are used to show how a larger category is divided into smaller subcategories and what the relationship of each part has on the total amount. There are two types of Stacked Bar Graphs: \n\n Simple Stacked Bar charts place each value for the segment after the previous one. The total value of the bar is all the segment values added together. Ideal for comparing the total amounts across each segmented bar. \n\n 100% Stack Bar Graphs show the percentage-of-the-whole by plotting the percentage of each value to the total amount in each group. This makes it easier to see the relative differences between quantities in each group. \n\n One major flaw of Stacked Bar Graphs is that they become harder to read the more segments each bar has. Also, comparing each segment to the other is difficult, as they're not aligned on a common baseline.",
     description:
-      "Stacked bar chart is used to compare values across a few categories. Use it when the order of categories is not important.",
+      "A Stacked Bar visualizes the composition of categories by stacking segments on top of each other within bars, showing both total and individual category contributions.",
     imageDescription: StackedBarChartDescriptionImage,
     imageFullDescription: StackedBarChartFullDescriptionImage,
     link: "https://datavizcatalogue.com/methods/stacked_bar_graph.html",
