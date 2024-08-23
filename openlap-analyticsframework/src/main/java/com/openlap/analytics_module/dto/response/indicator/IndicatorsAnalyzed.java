7f982cd88ddb78f671eebfaa044339884bd9412c
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-08-23 11:55:24 +0200

    Add analyzed indicator api

---
+++ openlap-analyticsframework/src/main/java/com/openlap/analytics_module/dto/response/indicator/IndicatorsAnalyzed.java
@@ -0,0 +1,14 @@
+package com.openlap.analytics_module.dto.response.indicator;
+
+import com.openlap.dataset.OpenLAPDataSet;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class IndicatorsAnalyzed {
+	private String id;
+	private OpenLAPDataSet analyzedDataset;
+}
---
+++ openlap-analyticsframework/src/main/java/com/openlap/analytics_module/dto/response/indicator/IndicatorsAnalyzedResponse.java
@@ -0,0 +1,16 @@
+package com.openlap.analytics_module.dto.response.indicator;
+
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class IndicatorsAnalyzedResponse {
+	private List<IndicatorsAnalyzed> indicators;
+}
+
