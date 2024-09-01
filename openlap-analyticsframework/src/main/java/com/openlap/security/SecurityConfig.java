9fba9672cdcabf62b9fe73f41b72dacde64c80f7
Shoeb Joarder <shoeb.joarder@uni-due.de>
2024-09-01 11:55:41 +0200

    Enable frame embedding

--- openlap-analyticsframework/src/main/java/com/openlap/security/SecurityConfig.java
+++ openlap-analyticsframework/src/main/java/com/openlap/security/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
+    http.headers().frameOptions().disable();
     http.sessionManagement().sessionCreationPolicy(STATELESS);
 
     // CORS configuration
