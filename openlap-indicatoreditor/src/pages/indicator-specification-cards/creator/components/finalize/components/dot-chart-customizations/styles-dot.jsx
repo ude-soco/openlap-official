54821ebdb762a553a5d7013cbaf0b5c6c99d57aa
jordyhz <61145500+jordyhz@users.noreply.github.com>
2025-03-14 01:00:48 +0100

    updated filter for dot chart ISC

--- openlap-indicatoreditor/src/pages/indicator-specification-cards/creator/components/finalize/components/dot-chart-customizations/styles-dot.jsx
+++ openlap-indicatoreditor/src/pages/indicator-specification-cards/creator/components/finalize/components/dot-chart-customizations/styles-dot.jsx
@@ -104,7 +104,7 @@ export const StylesDot = () => {
                     />
                   </Box>
                 </Grid>
-                <Grid item>
+                <Grid sx={{ mt: 0.5 }} item>
                   <Typography style={{ marginTop: "5px" }} variant="body">
                     {state.series[0]?.name}
                   </Typography>
@@ -157,7 +157,7 @@ export const StylesDot = () => {
                     />
                   </Box>
                 </Grid>
-                <Grid item>
+                <Grid sx={{ mt: 0.5 }} item>
                   <Typography style={{ marginTop: "5px" }} variant="body">
                     Data labels color
                   </Typography>
@@ -183,7 +183,7 @@ export const StylesDot = () => {
                     />
                   </Box>
                 </Grid>
-                <Grid item>
+                <Grid sx={{ mt: 0.5 }} item>
                   <Typography style={{ marginTop: "5px" }} variant="body">
                     Data labels fore color
                     <br />
