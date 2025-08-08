2778e137c6a43086fbd72ef566e7e18be626c503
Shoeb Joarder <shoeb.joarder@uni-due.de>
2025-08-08 17:20:57 +0200

    Add analysis summary

--- openlap-indicatoreditor/src/pages/indicators/indicator-editor/basic-indicator/components/analysis/components/analysis-summary.jsx
+++ openlap-indicatoreditor/src/pages/indicators/indicator-editor/basic-indicator/components/analysis/components/analysis-summary.jsx
@@ -5,9 +5,10 @@ import { BasicContext } from "../../../basic-indicator";
 import TipPopover from "../../../../../../../common/components/tip-popover/tip-popover";
 import ToggleSummaryButton from "../../../../../../../common/components/toggle-summary-button/toggle-summary-button";
 import { ToggleEditIconButton } from "../../../../../../../common/components/toggle-edit-button/toggle-edit-button";
+import AnalyzedDataTable from "./analyzed-data-table";
 
 export default function AnalysisSummary() {
-  const { lockedStep, setLockedStep } = useContext(BasicContext);
+  const { analysis, lockedStep, setLockedStep } = useContext(BasicContext);
   const [state, setState] = useState({
     tipAnchor: false,
     showSelections: true,
@@ -34,7 +35,7 @@ export default function AnalysisSummary() {
 
   return (
     <>
-      <Grid container spacing={1}>
+      <Grid container>
         <Grid size={{ xs: 12 }}>
           <Grid container justifyContent="space-between" spacing={1}>
             <Grid size="grow">
@@ -66,7 +67,45 @@ export default function AnalysisSummary() {
           unmountOnExit
         >
           <Grid size={{ xs: 12 }}>
-            <Typography>Analysis summary</Typography>
+            <Grid container spacing={1}>
+              <Grid size={{ xs: 12 }}>
+                <Grid container spacing={1} alignItems="center">
+                  <Typography>Selected Analytics Method</Typography>
+                  <Chip label={analysis.selectedAnalyticsMethod.method.name} />
+                </Grid>
+              </Grid>
+              <Grid size={{ xs: 12 }}>
+                <Grid container spacing={1} alignItems="center">
+                  <Typography>Selected Inputs</Typography>
+                  {analysis.inputs.map((input) => {
+                    if (input.selectedInput) {
+                      return (
+                        <Chip
+                          key={input.id}
+                          label={`${input.title} - ${input.selectedInput.name}`}
+                        />
+                      );
+                    }
+                  })}
+                </Grid>
+              </Grid>
+              <Grid size={{ xs: 12 }}>
+                <Grid container spacing={1} alignItems="center">
+                  <Typography>Selected Inputs</Typography>
+                  {analysis.params.map((param) => (
+                    <Chip
+                      key={param.id}
+                      label={`${param.title} (${param.value})`}
+                    />
+                  ))}
+                </Grid>
+              </Grid>
+              <Grid size={{ xs: 12 }}>
+                {Object.keys(analysis.analyzedData).length ? (
+                  <AnalyzedDataTable />
+                ) : undefined}
+              </Grid>
+            </Grid>
           </Grid>
         </Collapse>
       </Grid>
