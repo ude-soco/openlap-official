64d4cdae2a8f1cba72cc95c2fc35074d77c3bf38
Shoeb Joarder <shoeb.joarder@uni-due.de>
2025-08-12 12:10:07 +0200

    Update footer.jsx

--- openlap-indicatoreditor/src/common/components/footer/footer.jsx
+++ openlap-indicatoreditor/src/common/components/footer/footer.jsx
@@ -1,18 +1,18 @@
-import { Box, Grid, IconButton, Link, Typography } from "@mui/material";
+import { Box, IconButton, Link, Tooltip, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import { useContext } from "react";
+import { CustomThemeContext } from "../../../setup/theme-manager/theme-context-manager";
 
 const Footer = () => {
+  const { darkMode } = useContext(CustomThemeContext);
   return (
     <>
       <Grid
         container
-        sx={{
-          px: 2,
-          py: 1,
-        }}
+        sx={{ p: 2 }}
         justifyContent="space-between"
-        component={Box}
         alignItems="center"
       >
         <Grid item xs sx={{ zIndex: 1 }}>
@@ -34,20 +34,26 @@ const Footer = () => {
             <Typography sx={{ mr: 1 }} variant="body2">
               Follow us
             </Typography>
-            <IconButton
-              onClick={() =>
-                window.open(
-                  "https://www.youtube.com/channel/UCQV36Dfq-mfmAG0SqrQ_QbA"
-                )
-              }
-            >
-              <YouTubeIcon />
-            </IconButton>
-            <IconButton
-              onClick={() => window.open("https://github.com/ude-soco")}
-            >
-              <GitHubIcon />
-            </IconButton>
+            <Tooltip title="Visit our YouTube channel">
+              <IconButton
+                sx={{ color: "red" }}
+                onClick={() =>
+                  window.open(
+                    "https://www.youtube.com/channel/UCQV36Dfq-mfmAG0SqrQ_QbA"
+                  )
+                }
+              >
+                <YouTubeIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Visit our GitHub">
+              <IconButton
+                sx={{ color: darkMode ? "white" : "black" }}
+                onClick={() => window.open("https://github.com/ude-soco")}
+              >
+                <GitHubIcon />
+              </IconButton>
+            </Tooltip>
           </Grid>
         </Grid>
       </Grid>
