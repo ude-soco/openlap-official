63e753bf0ba2eeaa7dbace0288c88feb906da196
Ralf Berger <ralf.berger@uni-due.de>
2025-08-27 14:04:56 +0200

    Clean up

--- openlap-analyticsframework/src/main/java/com/openlap/security/config/CorsConfig.java
+++ openlap-analyticsframework/src/main/java/com/openlap/security/config/CorsConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 public class CorsConfig {
