0b1e61d51593a03d486e8fc78bc99f2df049663e
Shoeb Joarder <shoeb.joarder@uni-due.de>
2025-09-02 13:12:52 +0200

    Update add lrs management dialogs

--- openlap-indicatoreditor/src/pages/account-manager/components/add-lrs-consumer.jsx
+++ openlap-indicatoreditor/src/pages/account-manager/components/add-lrs-consumer.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import {
   Button,
   Dialog,
@@ -6,7 +6,6 @@ import {
   DialogContent,
   DialogTitle,
   FormControl,
-  Grid,
   InputLabel,
   MenuItem,
   Select,
@@ -14,6 +13,7 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import { LoadingButton } from "@mui/lab";
 import { Alert } from "@mui/material";
 import { useSnackbar } from "notistack";
@@ -21,7 +21,7 @@ import {
   requestAvailableLrsInOpenLAP,
   requestLrsConsumerValidation,
 } from "../utils/account-manager-api.js";
-import { AuthContext } from "../../../setup/auth-context-manager/auth-context-manager.jsx";
+import { AuthContext } from "../../../setup/auth-context-manager/auth-context-manager";
 
 const AddLrsConsumer = ({ addLrsConsumer, toggleOpen }) => {
   const { api } = useContext(AuthContext);
@@ -38,41 +38,28 @@ const AddLrsConsumer = ({ addLrsConsumer, toggleOpen }) => {
   const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
-    const loadLRSData = async (api) => {
-      try {
-        return await requestAvailableLrsInOpenLAP(api);
-      } catch (error) {
-        throw error;
-      }
-    };
-    setState((prevState) => ({
-      ...prevState,
-      loadingConsumerLrs: true,
-    }));
-    loadLRSData(api)
-      .then((response) => {
-        setState((prevState) => ({
-          ...prevState,
-          allAvailableLrs: response,
-          loadingConsumerLrs: false,
-        }));
-      })
-      .catch((error) => {
-        setState((prevState) => ({
-          ...prevState,
-          loadingConsumerLrs: false,
-        }));
-        enqueueSnackbar(error.response.message, { variant: "error" });
-      });
+    loadLRSData();
   }, [addLrsConsumer.open]);
+
+  const loadLRSData = async () => {
+    setState((p) => ({ ...p, loadingConsumerLrs: true }));
+    try {
+      const availableLRS = await requestAvailableLrsInOpenLAP(api);
+      setState((p) => ({ ...p, allAvailableLrs: availableLRS }));
+    } catch (error) {
+      enqueueSnackbar(error.response.message, { variant: "error" });
+    } finally {
+      setState((p) => ({ ...p, loadingConsumerLrs: false }));
+    }
+  };
 
   const handleLrsConsumerRequest = (event) => {
     const { name, value } = event.target;
-    setState((prevState) => ({
-      ...prevState,
+    setState((p) => ({
+      ...p,
       [name]: value,
       validation: {
-        ...prevState.validation,
+        ...p.validation,
         status: undefined,
         message: "",
       },
@@ -80,93 +67,75 @@ const AddLrsConsumer = ({ addLrsConsumer, toggleOpen }) => {
   };
 
   const handleConfirm = async () => {
-    setState((prevState) => ({
-      ...prevState,
-      validation: {
-        ...prevState.validation,
-        loading: true,
-      },
-    }));
+    setState((p) => ({ ...p, validation: { ...p.validation, loading: true } }));
 
-    await requestLrsConsumerValidation(api, state.lrsId, state.uniqueIdentifier)
-      .then((response) => {
-        setState((prevState) => ({
-          ...prevState,
+    try {
+      const validationResponse = await requestLrsConsumerValidation(
+        api,
+        state.lrsId,
+        state.uniqueIdentifier
+      );
+      setState((p) => ({
+        ...p,
+        validation: {
+          ...p.validation,
+          loading: false,
+          status: validationResponse.data,
+        },
+      }));
+      toggleOpen(true, validationResponse.message);
+    } catch (error) {
+      if (error.response.status === 404) {
+        setState((p) => ({
+          ...p,
           validation: {
-            ...prevState.validation,
+            ...p.validation,
             loading: false,
-            status: response.data,
+            status: false,
+            message: error.response.data.message,
           },
         }));
-        toggleOpen(true, response.message);
-      })
-      .catch((error) => {
-        if (error.response.status === 404) {
-          setState((prevState) => ({
-            ...prevState,
-            validation: {
-              ...prevState.validation,
-              loading: false,
-              status: false,
-              message: error.response.data.message,
-            },
-          }));
-        }
-      });
+      }
+    }
   };
 
   return (
     <>
-      <Dialog
-        open={addLrsConsumer.open}
-        // onClose={toggleOpen}
-        maxWidth="sm"
-        fullWidth
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">Add new LRS</DialogTitle>
+      <Dialog open={addLrsConsumer.open} maxWidth="sm" fullWidth>
+        <DialogTitle>Add new LRS</DialogTitle>
         <DialogContent>
-          <Grid container spacing={2} sx={{ pt: 1 }}>
+          <Grid container direction="column" spacing={2} sx={{ pt: 1 }}>
             {state.loadingConsumerLrs ? (
-              <Grid item xs={12}>
-                <Skeleton variant="rectangular" height={200} width="100%" />
-              </Grid>
+              <Skeleton variant="rectangular" height={200} width="100%" />
             ) : state.allAvailableLrs.length > 0 ? (
               <>
-                <Grid item xs={12}>
-                  <FormControl fullWidth>
-                    <InputLabel id="lrs-select-label">Available LRS</InputLabel>
-                    <Select
-                      name="lrsId"
-                      value={state.lrsId}
-                      label="LRS ID"
-                      onChange={handleLrsConsumerRequest}
-                    >
-                      {state.allAvailableLrs.map((lrs) => {
-                        return (
-                          <MenuItem key={lrs.lrsId} value={lrs.lrsId}>
-                            {lrs.title}
-                          </MenuItem>
-                        );
-                      })}
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    name="uniqueIdentifier"
-                    label="Unique Identifier"
-                    placeholder="Unique Identifier"
+                <FormControl fullWidth>
+                  <InputLabel>Available LRS</InputLabel>
+                  <Select
+                    name="lrsId"
+                    value={state.lrsId}
+                    label="Available LRS"
                     onChange={handleLrsConsumerRequest}
-                  />
-                </Grid>
+                  >
+                    {state.allAvailableLrs.map((lrs) => {
+                      return (
+                        <MenuItem key={lrs.lrsId} value={lrs.lrsId}>
+                          {lrs.title}
+                        </MenuItem>
+                      );
+                    })}
+                  </Select>
+                </FormControl>
+                <TextField
+                  fullWidth
+                  name="uniqueIdentifier"
+                  label="Unique Identifier"
+                  placeholder="Unique Identifier"
+                  onChange={handleLrsConsumerRequest}
+                />
                 {state.validation.status !== undefined &&
                   !state.validation.status && (
-                    <Grid item xs={12}>
-                      <Alert severity="error">{state.validation.message}</Alert>
-                    </Grid>
+                    <Alert severity="error">{state.validation.message}</Alert>
                   )}
               </>
             ) : (
@@ -175,29 +144,22 @@ const AddLrsConsumer = ({ addLrsConsumer, toggleOpen }) => {
           </Grid>
         </DialogContent>
         <DialogActions>
-          <Grid container spacing={1} justifyContent="space-between">
-            <Grid item xs>
-              <Button fullWidth onClick={toggleOpen}>
-                Cancel
-              </Button>
-            </Grid>
-            <Grid item xs>
-              <LoadingButton
-                loading={state.validation.loading}
-                loadingIndicator="Adding…"
-                fullWidth
-                autoFocus
-                onClick={handleConfirm}
-                variant="contained"
-                disabled={
-                  !Boolean(state.lrsId.length) ||
-                  !Boolean(state.uniqueIdentifier)
-                }
-              >
-                Add LRS
-              </LoadingButton>
-            </Grid>
-          </Grid>
+          <Button fullWidth onClick={toggleOpen}>
+            Cancel
+          </Button>
+          <LoadingButton
+            loading={state.validation.loading}
+            loadingIndicator="Adding…"
+            fullWidth
+            autoFocus
+            onClick={handleConfirm}
+            variant="contained"
+            disabled={
+              !Boolean(state.lrsId.length) || !Boolean(state.uniqueIdentifier)
+            }
+          >
+            Add LRS
+          </LoadingButton>
         </DialogActions>
       </Dialog>
     </>
--- openlap-indicatoreditor/src/pages/account-manager/components/add-lrs-provider.jsx
+++ openlap-indicatoreditor/src/pages/account-manager/components/add-lrs-provider.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from "react";
-import { AuthContext } from "../../../setup/auth-context-manager/auth-context-manager.jsx";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../setup/auth-context-manager/auth-context-manager";
 import {
   Button,
   Dialog,
@@ -7,12 +7,12 @@ import {
   DialogContent,
   DialogTitle,
   FormControl,
-  Grid,
   InputLabel,
   MenuItem,
   Select,
   TextField,
 } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import UniqueIdentifierTypes from "../utils/enums/unique-identifier-types.js";
 import { requestCreateLRSProvider } from "../utils/account-manager-api.js";
 
@@ -25,20 +25,16 @@ const AddLrsProvider = ({ addLrsProvider, toggleOpen }) => {
 
   const handleLrsDetails = (event) => {
     const { name, value } = event.target;
-    setState(() => ({
-      ...state,
-      [name]: value,
-    }));
+    setState(() => ({ ...state, [name]: value }));
   };
 
   const handleConfirm = async () => {
-    await requestCreateLRSProvider(
+    const response = await requestCreateLRSProvider(
       api,
       state.title,
-      state.uniqueIdentifierType,
-    ).then((response) => {
-      toggleOpen(true, response.message);
-    });
+      state.uniqueIdentifierType
+    );
+    toggleOpen(true, response.message);
   };
 
   return (
@@ -52,59 +48,47 @@ const AddLrsProvider = ({ addLrsProvider, toggleOpen }) => {
       >
         <DialogTitle id="alert-dialog-title">Create new LRS</DialogTitle>
         <DialogContent>
-          <Grid container spacing={2} sx={{ pt: 1 }}>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                name="title"
-                label="LRS name"
-                placeholder="Name your LRS"
+          <Grid container direction="column" spacing={2} sx={{ pt: 1 }}>
+            <TextField
+              fullWidth
+              name="title"
+              label="LRS name"
+              placeholder="Name your LRS"
+              onChange={handleLrsDetails}
+            />
+            <FormControl fullWidth>
+              <InputLabel>Unique Identifier Type</InputLabel>
+              <Select
+                labelId="unique-type-select-label"
+                name="uniqueIdentifierType"
+                value={state.uniqueIdentifierType}
+                label="Unique Identifier Type"
                 onChange={handleLrsDetails}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl fullWidth>
-                <InputLabel id="unique-type-select-label">
-                  Unique Identifier Type
-                </InputLabel>
-                <Select
-                  labelId="unique-type-select-label"
-                  name="uniqueIdentifierType"
-                  value={state.uniqueIdentifierType}
-                  label="Unique Identifier Type"
-                  onChange={handleLrsDetails}
-                >
-                  {Object.entries(UniqueIdentifierTypes).map(([key, value]) => {
-                    return (
-                      <MenuItem key={key} value={value}>
-                        {key}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
-            </Grid>
+              >
+                {Object.entries(UniqueIdentifierTypes).map(([key, value]) => {
+                  return (
+                    <MenuItem key={key} value={value}>
+                      {key}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
           </Grid>
         </DialogContent>
         <DialogActions>
-          <Grid container spacing={1} justifyContent="space-between">
-            <Grid item xs>
-              <Button fullWidth onClick={toggleOpen}>
-                Cancel
-              </Button>
-            </Grid>
-            <Grid item xs>
-              <Button
-                fullWidth
-                autoFocus
-                onClick={handleConfirm}
-                variant="contained"
-                disabled={!Boolean(state.title.length)}
-              >
-                Create LRS
-              </Button>
-            </Grid>
-          </Grid>
+          <Button fullWidth onClick={toggleOpen}>
+            Cancel
+          </Button>
+          <Button
+            fullWidth
+            autoFocus
+            onClick={handleConfirm}
+            variant="contained"
+            disabled={!Boolean(state.title.length)}
+          >
+            Create LRS
+          </Button>
         </DialogActions>
       </Dialog>
     </>
