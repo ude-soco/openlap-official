ec030385ff7261d5718ecc7b3df82fcfbbca6f50
Shoeb Joarder <shoeb.joarder@uni-due.de>
2025-09-02 11:30:59 +0200

    Improve news section

--- openlap-indicatoreditor/src/pages/landing-page/components/news.jsx
+++ openlap-indicatoreditor/src/pages/landing-page/components/news.jsx
@@ -1,15 +1,31 @@
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid2";
+import Typography from "@mui/material/Typography";
+import { Button, Link, Paper } from "@mui/material";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineOppositeContent,
+} from "@mui/lab";
 import { navigationIds } from "../utils/navigation-data";
 import { newsItems } from "../utils/news-data";
-import { Button } from "@mui/material";
-import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useContext } from "react";
+import { CustomThemeContext } from "../../../setup/theme-manager/theme-context-manager";
 
 export default function News() {
+  const { theme } = useContext(CustomThemeContext);
+
+  const sortedNews = [...newsItems].sort(
+    (a, b) => new Date(b.date) - new Date(a.date)
+  );
+
   return (
     <Container
       id={navigationIds.NEWS}
@@ -37,35 +53,55 @@ export default function News() {
           Analytics Platform
         </Typography>
       </Box>
-      <Grid container direction="column" spacing={2}>
-        {newsItems.map((item) => (
-          <Grid size={{ xs: 12 }} key={item.id}>
-            <Card variant="outlined">
-              <CardContent>
-                <Typography variant="body2" gutterBottom>
-                  {item.date}
+
+      <Timeline position="alternate">
+        {sortedNews.map((item, index) => (
+          <TimelineItem key={item.id} sx={{ pb: 1 }}>
+            <TimelineOppositeContent color="textSecondary">
+              <Typography variant="body2" color="text.secondary" gutterBottom>
+                {item.date}
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="primary" />
+              {index < sortedNews.length - 1 && <TimelineConnector />}
+            </TimelineSeparator>
+            <TimelineContent>
+              <Box component={Paper} sx={{ p: 2 }} variant="outlined">
+                <Typography fontWeight="medium" gutterBottom>
+                  {item.title}
                 </Typography>
-                <Typography fontWeight="medium">{item.title}</Typography>
-                <Typography color="inherit" variant="body2" gutterBottom>
+                <Typography variant="body2" color="text.primary" gutterBottom>
                   {item.desc}
                 </Typography>
-
-                <Typography variant="body2" sx={{ color: "grey.400" }}>
-                  {item.venue}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
+                {item.venue && (
+                  <Typography variant="body2" sx={{ color: "grey.600" }}>
+                    {item.venue}
+                  </Typography>
+                )}
+              </Box>
+            </TimelineContent>
+          </TimelineItem>
         ))}
-      </Grid>
-      <Grid container justifyContent="space-between">
-        <Button
-          endIcon={<OpenInNewIcon />}
+      </Timeline>
+
+      <Box>
+        <Link
+          component="button"
+          color="primary"
+          fontWeight="bold"
+          sx={{
+            display: "inline-flex",
+            alignItems: "center",
+            "& > svg": { transition: "0.2s" },
+            "&:hover > svg": { transform: "translateX(4px)" },
+          }}
           onClick={() => window.open("https://www.uni-due.de/soco/news.php")}
         >
-          More news
-        </Button>
-      </Grid>
+          <span>More News</span>
+          <OpenInNewIcon fontSize="small" sx={{ mt: "1px", ml: "2px" }} />
+        </Link>
+      </Box>
     </Container>
   );
 }
