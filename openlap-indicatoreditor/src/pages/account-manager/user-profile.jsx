07d60ac63455fbdd504b30a34bc4aa3126eefb31
Shoeb Joarder <shoeb.joarder@uni-due.de>
2025-09-02 13:54:41 +0200

    Update user profile component

--- openlap-indicatoreditor/src/pages/account-manager/user-profile.jsx
+++ openlap-indicatoreditor/src/pages/account-manager/user-profile.jsx
@@ -1,27 +1,31 @@
-import React, { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../setup/auth-context-manager/auth-context-manager.jsx";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../setup/auth-context-manager/auth-context-manager";
 import {
+  Breadcrumbs,
   Button,
   Chip,
   Divider,
-  Grid,
   IconButton,
+  Link,
   List,
   ListItemButton,
   ListItemText,
   Paper,
   Skeleton,
+  Stack,
   TextField,
   Typography,
 } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import { Link as RouterLink } from "react-router-dom";
 import { requestUserDetails } from "./utils/account-manager-api.js";
 import { Edit } from "@mui/icons-material";
 
 const UserProfile = () => {
-  const { logout, api, user } = useContext(AuthContext);
+  const { api } = useContext(AuthContext);
 
   const [state, setState] = useState({
-    loading: true,
+    loading: false,
     page: 1,
     user: {
       name: "",
@@ -34,55 +38,46 @@ const UserProfile = () => {
   });
 
   useEffect(() => {
-    const loadData = async () => {
-      try {
-        return await requestUserDetails(api);
-      } catch (error) {
-        console.error("Failed to load user data", error);
-      }
-    };
-    loadData().then((response) => {
-      setState((prevState) => ({
-        ...prevState,
-        user: {
-          ...prevState.user,
-          ...response,
-        },
-        loading: false,
-      }));
-    });
+    loadUserData();
   }, []);
 
+  const loadUserData = async () => {
+    setState((p) => ({ ...p, loading: true }));
+    try {
+      const userData = await requestUserDetails(api);
+      setState((p) => ({ ...p, user: { ...p.user, ...userData } }));
+    } catch (error) {
+      console.error("Error loading user data", error);
+    } finally {
+      setState((p) => ({ ...p, loading: false }));
+    }
+  };
+
   const togglePage = (pageNumber) => {
-    setState((prevState) => ({
-      ...prevState,
-      page: pageNumber,
-    }));
+    setState((p) => ({ ...p, page: pageNumber }));
   };
 
   const handleFormFields = (event) => {
     const { name, value } = event.target;
-    setState((prevState) => ({
-      ...state,
-      user: {
-        ...prevState.user,
-        [name]: value,
-      },
-    }));
+    setState((p) => ({ ...p, user: { ...p.user, [name]: value } }));
   };
 
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography>Account Settings</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Divider />
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container spacing={2} sx={{ pt: 5 }} justifyContent="center">
-            <Grid item xs={12} md={2}>
+      <Grid container direction="column" spacing={2}>
+        <Breadcrumbs>
+          <Link component={RouterLink} underline="hover" color="inherit" to="/">
+            Home
+          </Link>
+          <Typography sx={{ color: "text.primary" }}>
+            Account Settings
+          </Typography>
+        </Breadcrumbs>
+
+        <Divider />
+        <Grid container justifyContent="center" sx={{ pt: 3 }}>
+          <Grid size={{ xs: 12, sm: 8 }}>
+            <Stack direction="row" spacing={2}>
               <Grid container direction="column">
                 <Paper variant="outlined" component="form">
                   <List component="div" disablePadding>
@@ -104,88 +99,57 @@ const UserProfile = () => {
                   </List>
                 </Paper>
               </Grid>
-            </Grid>
-            {state.page === 1 && (
-              <Grid item xs={12} md={6}>
-                <Paper variant="outlined" sx={{ p: 3 }}>
-                  <Grid container justifyContent="center" spacing={2}>
-                    <Grid item xs={12}>
-                      <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                          <Typography variant="h5">
-                            {state.user.name ? (
-                              `Hello, ${state.user.name}`
-                            ) : (
-                              <Skeleton />
-                            )}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                          <Grid container spacing={1} alignItems="center">
-                            <Grid item>
-                              <Typography>Email:</Typography>
-                            </Grid>
-                            {state.user.email ? (
-                              <>
-                                <Grid item>
-                                  <Chip label={state.user.email} />
-                                </Grid>
-                                <Grid item>
-                                  <IconButton size="small">
-                                    <Edit />
-                                  </IconButton>
-                                </Grid>
-                              </>
-                            ) : undefined}
-                          </Grid>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Paper>
-              </Grid>
-            )}
-            {state.page === 2 && (
-              <Grid item xs={12} md={6}>
-                <Paper variant="outlined" sx={{ p: 3 }}>
-                  <Grid container justifyContent="center" spacing={2}>
-                    <Grid item xs={12}>
-                      <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                          <Typography>Change password</Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                          <TextField
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            placeholder="Password"
-                            type="password"
-                            onChange={handleFormFields}
-                          />
-                        </Grid>
-                        <Grid item xs={12}>
-                          <TextField
-                            fullWidth
-                            name="confirmPassword"
-                            label="Confirm Password"
-                            placeholder="Confirm password"
-                            type="password"
-                            onChange={handleFormFields}
-                          />
-                        </Grid>
-                      </Grid>
-                    </Grid>
 
-                    <Grid item xs={12}>
-                      <Button variant="contained" fullWidth disabled>
-                        Update
-                      </Button>
+              {state.page === 1 && (
+                <Paper variant="outlined" sx={{ p: 3, width: "100%" }}>
+                  <Grid container direction="column" spacing={2}>
+                    <Typography variant="h5">
+                      {state.user.name ? (
+                        `Hello, ${state.user.name}`
+                      ) : (
+                        <Skeleton />
+                      )}
+                    </Typography>
+                    <Grid container spacing={1} alignItems="center">
+                      <Typography>Email:</Typography>
+                      <Chip label={state.user.email} />
+                      <IconButton size="small">
+                        <Edit />
+                      </IconButton>
                     </Grid>
                   </Grid>
                 </Paper>
-              </Grid>
-            )}
+              )}
+              {state.page === 2 && (
+                <Paper variant="outlined" sx={{ p: 3, width: "100%" }}>
+                  <Stack spacing={2}>
+                    <Typography>Change password</Typography>
+                    <Stack direction="row" spacing={2}>
+                      <TextField
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        placeholder="Password"
+                        type="password"
+                        onChange={handleFormFields}
+                      />
+                      <TextField
+                        fullWidth
+                        name="confirmPassword"
+                        label="Confirm Password"
+                        placeholder="Confirm password"
+                        type="password"
+                        onChange={handleFormFields}
+                      />
+                    </Stack>
+
+                    <Button variant="contained" fullWidth disabled>
+                      Update
+                    </Button>
+                  </Stack>
+                </Paper>
+              )}
+            </Stack>
           </Grid>
         </Grid>
       </Grid>
