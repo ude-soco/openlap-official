bda6aa54448cbf7fc2d200e7dfe94e050bb7c909
Shoeb Joarder <shoeb.joarder@uni-due.de>
2025-09-16 09:51:45 +0200

    Fix padding footer

--- openlap-indicatoreditor/src/common/components/footer/footer.jsx
+++ openlap-indicatoreditor/src/common/components/footer/footer.jsx
@@ -1,4 +1,4 @@
-import { IconButton, Link, Grid, Tooltip, Typography } from "@mui/material";
+import { IconButton, Link, Tooltip, Typography, Stack } from "@mui/material";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import { useContext } from "react";
@@ -8,51 +8,48 @@ const Footer = () => {
   const { darkMode } = useContext(CustomThemeContext);
   return (
     <>
-      <Grid
-        container
+      <Stack
+        direction="row"
         justifyContent="space-between"
         alignItems="center"
-        spacing={1}
+        gap={1}
+        sx={{ p: 2 }}
       >
-        <Grid size={{ xs: 8 }}>
-          <Typography variant="body2">
-            {"Copyright @ "}
-            <Link
-              color="inherit"
-              href="https://www.uni-due.de/soco"
-              target="_blank"
+        <Typography variant="body2">
+          {"Copyright @ "}
+          <Link
+            color="inherit"
+            href="https://www.uni-due.de/soco"
+            target="_blank"
+          >
+            Social Computing Group
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+        <Stack direction="row" gap={1} alignItems="center">
+          <Tooltip title="Visit our YouTube channel">
+            <IconButton
+              color="primary"
+              onClick={() =>
+                window.open(
+                  "https://www.youtube.com/channel/UCQV36Dfq-mfmAG0SqrQ_QbA"
+                )
+              }
             >
-              Social Computing Group
-            </Link>{" "}
-            {new Date().getFullYear()}
-            {"."}
-          </Typography>
-        </Grid>
-        <Grid size="auto">
-          <Grid container spacing={1} alignItems="center">
-            <Tooltip title="Visit our YouTube channel">
-              <IconButton
-                color="primary"
-                onClick={() =>
-                  window.open(
-                    "https://www.youtube.com/channel/UCQV36Dfq-mfmAG0SqrQ_QbA"
-                  )
-                }
-              >
-                <YouTubeIcon />
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Visit our GitHub">
-              <IconButton
-                color="primary"
-                onClick={() => window.open("https://github.com/ude-soco")}
-              >
-                <GitHubIcon />
-              </IconButton>
-            </Tooltip>
-          </Grid>
-        </Grid>
-      </Grid>
+              <YouTubeIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Visit our GitHub">
+            <IconButton
+              color="primary"
+              onClick={() => window.open("https://github.com/ude-soco")}
+            >
+              <GitHubIcon />
+            </IconButton>
+          </Tooltip>
+        </Stack>
+      </Stack>
     </>
   );
 };
