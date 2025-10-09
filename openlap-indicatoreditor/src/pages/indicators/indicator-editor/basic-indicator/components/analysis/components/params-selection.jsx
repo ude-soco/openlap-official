0a55db1f77e5fa826c6c272c88fd48042b6f8326
Shoeb Joarder <shoeb.joarder@uni-due.de>
2025-10-09 11:33:58 +0200

    Update param selection with stacks

--- openlap-indicatoreditor/src/pages/indicators/indicator-editor/basic-indicator/components/analysis/components/params-selection.jsx
+++ openlap-indicatoreditor/src/pages/indicators/indicator-editor/basic-indicator/components/analysis/components/params-selection.jsx
@@ -3,12 +3,12 @@ import { BasicContext } from "../../../basic-indicator";
 import {
   FormControl,
   InputLabel,
-  Grid,
   MenuItem,
   Paper,
   Select,
   TextField,
   Typography,
+  Stack,
 } from "@mui/material";
 import CustomTooltip from "../../../../../../../common/components/custom-tooltip/custom-tooltip";
 
@@ -28,14 +28,8 @@ export default function ParamsSelection() {
   };
 
   return (
-    <Grid
-      container
-      spacing={2}
-      component={Paper}
-      variant="outlined"
-      sx={{ p: 2 }}
-    >
-      <Grid container spacing={0} alignItems="center">
+    <Stack gap={2} component={Paper} variant="outlined" sx={{ p: 2 }}>
+      <Stack direction="row" alignItems="center">
         <Typography>
           Set <b>Parameters</b> of the method
         </Typography>
@@ -44,57 +38,49 @@ export default function ParamsSelection() {
           message={`Parameters are preselected with default values and can be changed
               based on your needs.`}
         />
-      </Grid>
-      <Grid container spacing={2} alignItems="center">
-        {analysis.params.map((param, index) => (
-          <Grid size={{ xs: 12 }} key={index}>
-            <Grid container alignItems="center" spacing={1}>
-              <Grid size="grow">
-                <FormControl fullWidth>
-                  {param.type === PARAM_TYPE.choice && (
-                    <>
-                      <InputLabel required={Boolean(param.required)}>
-                        {param.title}
-                      </InputLabel>
-                      <Select
-                        label={param.title}
-                        value={
-                          param.value !== undefined && param.value !== null
-                            ? param.value
-                            : param.defaultValue
-                        }
-                        onChange={(event) => handleChangeParam(event, param)}
-                      >
-                        {param.possibleValues.split(",").map((value, index) => (
-                          <MenuItem key={index} value={value}>
-                            {value}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    </>
-                  )}
-                  {param.type === PARAM_TYPE.textBox && (
-                    <TextField
-                      value={
-                        param.value !== undefined && param.value !== null
-                          ? param.value
-                          : param.defaultValue || ""
-                      }
-                      type={param.dataType === "INTEGER" ? "number" : "text"}
-                      required={Boolean(param.required)}
-                      label={param.title}
-                      onChange={(event) => handleChangeParam(event, param)}
-                    />
-                  )}
-                </FormControl>
-              </Grid>
-              <Grid size="auto">
-                <CustomTooltip type="description" message={param.description} />
-              </Grid>
-            </Grid>
-          </Grid>
-        ))}
-      </Grid>
-    </Grid>
+      </Stack>
+      {analysis.params.map((param, index) => (
+        <Stack gap={1} direction="row" alignItems="center">
+          <FormControl fullWidth>
+            {param.type === PARAM_TYPE.choice && (
+              <>
+                <InputLabel required={Boolean(param.required)}>
+                  {param.title}
+                </InputLabel>
+                <Select
+                  label={param.title}
+                  value={
+                    param.value !== undefined && param.value !== null
+                      ? param.value
+                      : param.defaultValue
+                  }
+                  onChange={(event) => handleChangeParam(event, param)}
+                >
+                  {param.possibleValues.split(",").map((value, index) => (
+                    <MenuItem key={index} value={value}>
+                      {value}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </>
+            )}
+            {param.type === PARAM_TYPE.textBox && (
+              <TextField
+                value={
+                  param.value !== undefined && param.value !== null
+                    ? param.value
+                    : param.defaultValue || ""
+                }
+                type={param.dataType === "INTEGER" ? "number" : "text"}
+                required={Boolean(param.required)}
+                label={param.title}
+                onChange={(event) => handleChangeParam(event, param)}
+              />
+            )}
+          </FormControl>
+          <CustomTooltip type="description" message={param.description} />
+        </Stack>
+      ))}
+    </Stack>
   );
 }
